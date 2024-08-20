dcd8a26e99cb4257aceb9e737a4cc759e119be98
rud <redimredim1899@gmail.com>
2024-08-21 01:23:06 +0800

    1st commit

---
+++ scripts/test.js
@@ -0,0 +1,41 @@
+function apiBtnListener(element) {
+  const apiBtn = document.querySelectorAll(element);
+
+  apiBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      console.log("asd");
+      disabledButton(btn);
+    });
+  });
+}
+
+function disabledButton(btn) {
+  btn.disabled = true;
+  btn.textContent = "Please wait..."; // Change the button text
+  setTimeout(() => {
+    btn.disabled = false;
+    btn.textContent = "can generate now";
+  }, 5000);
+}
+
+async function callPokemonData() {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${1}`);
+
+    const data = await response.json();
+
+    let objectData = {
+      id: data.id,
+      name: data.name,
+      base_experience: data.base_experience,
+      height: data.height,
+      weight: data.weight,
+    };
+    return objectData;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
+}
+
+apiBtnListener(".apiBtn");
